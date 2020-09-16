372e3c5b7a0eca27ea0da31911a34d638c236689
Jake Stucky <jakestucky@gmnail.com>
2020-09-16 14:36:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cohortName: 'Tarjan',
+    salutation: 'Hello',
+    name: 'Jake',
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.cohortName}Learns State ith Object</h1>
+        <div>
+          {this.state.salutation} my name is {this.state.name}
+        </div>
       </div>
     );
   }
